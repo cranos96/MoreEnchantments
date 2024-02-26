47f56faa36c2b527ad0a72f793b8a21320657f5f
FengG0d <1320629797@qq.com>
2024-02-26 20:50:53 +0800

    无限（不死图腾）

--- src/main/java/cn/feng/enchant/mixin/MixinEntity.java
+++ src/main/java/cn/feng/enchant/mixin/MixinEntity.java
@@ -2,6 +2,8 @@ package cn.feng.enchant.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -11,5 +13,9 @@ import org.spongepowered.asm.mixin.Shadow;
  **/
 @Mixin(Entity.class)
 public abstract class MixinEntity {
+    @Shadow public abstract World getWorld();
+
+    @Shadow public abstract void emitGameEvent(GameEvent event);
+
     @Shadow public abstract BlockPos getBlockPos();
 }
--- src/main/java/cn/feng/enchant/mixin/MixinLivingEntity.java
+++ src/main/java/cn/feng/enchant/mixin/MixinLivingEntity.java
@@ -1,12 +1,24 @@
 package cn.feng.enchant.mixin;
 
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.Items;
+import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,10 +30,57 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends MixinEntity {
 
+    @Shadow public abstract ItemStack getStackInHand(Hand hand);
+
+    @Shadow public abstract void setHealth(float health);
+
+    @Shadow public abstract boolean clearStatusEffects();
+
+    @Shadow public abstract boolean addStatusEffect(StatusEffectInstance effect);
+
+    /**
+     * Infinity food
+     */
     @Inject(method = "eatFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V"))
     private void increaseFood(World world, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
         if (EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0) {
             stack.increment(1);
         }
+    }
+
+    /**
+     * @author ChengFeng
+     * @reason Infinity totem
+     */
+    @Overwrite
+    private boolean tryUseTotem(DamageSource source) {
+        if (source.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY)) {
+            return false;
+        }
+        ItemStack itemStack = null;
+        for (Hand hand : Hand.values()) {
+            ItemStack itemStack2 = this.getStackInHand(hand);
+            if (!itemStack2.isOf(Items.TOTEM_OF_UNDYING)) continue;
+            itemStack = itemStack2.copy();
+            if (EnchantmentHelper.getLevel(Enchantments.INFINITY, itemStack2) == 0) {
+                itemStack2.decrement(1);
+            }
+            break;
+        }
+        if (itemStack != null) {
+            LivingEntity livingEntity = (LivingEntity) (Object) this;
+            if (livingEntity instanceof ServerPlayerEntity serverPlayerEntity) {
+                serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(Items.TOTEM_OF_UNDYING));
+                Criteria.USED_TOTEM.trigger(serverPlayerEntity, itemStack);
+                this.emitGameEvent(GameEvent.ITEM_INTERACT_FINISH);
+            }
+            this.setHealth(1.0f);
+            this.clearStatusEffects();
+            this.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 900, 1));
+            this.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 100, 1));
+            this.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 800, 0));
+            this.getWorld().sendEntityStatus(livingEntity, EntityStatuses.USE_TOTEM_OF_UNDYING);
+        }
+        return itemStack != null;
     }
 }
